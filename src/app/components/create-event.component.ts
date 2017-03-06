df7a9e1c5f4b5071d7d49ba3005a1fb9b3fc7483
Juan Cruz Leyba <juancruz.leyba@santexgroup.com>
2017-03-06 16:55:44 -0300

    Added rendering methods for single event.

--- src/app/components/create-event.component.ts
+++ src/app/components/create-event.component.ts
@@ -14,7 +14,13 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class CreateEventComponent {
     myForm: FormGroup;
     user: User;
+    numberOfPlayers: Array<Number> = [2, 3, 4];
+    teeTimesIntervals: any = [];
+    hours: any = [];
+    minutes: any = [];
     showSpinner: boolean = false;
+    showAfternoon: boolean = false;
+    addTee: boolean = false;
 
     constructor(private authService: AuthService,
                 private eventService: EventsService,
@@ -22,10 +28,23 @@ export class CreateEventComponent {
                 private fb: FormBuilder) {
         this.initWithUser = this.initWithUser.bind(this);
         this.initWithUser();
+        this.initDropdowns();
 
         this.myForm = this.fb.group({
             name: ['', Validators.required],
             date: ['', Validators.required],
+            teeTimesIntervals: ['', Validators.required],
+            numberOfPlayers: ['', Validators.required],
+            morningShiftStartHours: ['', Validators.required],
+            morningShiftStartMinutes: ['', Validators.required],
+            morningShiftEndHours: ['', Validators.required],
+            morningShiftEndMinutes: ['', Validators.required],
+            afternoonShiftStartHours: [''],
+            afternoonShiftStartMinutes: [''],
+            afternoonShiftEndHours: [''],
+            afternoonShiftEndMinutes: [''],
+            showAfternoonInput: [''],
+            addTee: [''],
         });
     }
 
@@ -45,16 +64,43 @@ export class CreateEventComponent {
         }
     }
 
+    initDropdowns() {
+        for (var i = 1; i < 31; i++) {
+            this.teeTimesIntervals.push(i);
+        }
+        for (var i = 0; i < 24; i++) {
+            this.hours.push(i);
+        }
+        for (var i = 0; i < 60; i++) {
+            this.minutes.push(i);
+        }
+    }
+
+    formatTime(date: any, hours: any, minutes: any) {
+        date.setHours(hours);
+        date.setMinutes(minutes);
+        return date.getTime();
+    }
+
     addEvent() {
         this.showSpinner = true;
         var value = this.myForm.value;
+        var date = new Date(value.date);
         var event = {
             name: value.name,
-            date: new Date(value.date).getTime()
+            date: date.getTime(),
+            numberOfPlayers: value.numberOfPlayers,
+            teeTimesInterval: value.teeTimesIntervals,
+            morningShiftStart: this.formatTime(date, value.morningShiftStartHours, value.morningShiftStartMinutes),
+            morningShiftEnd: this.formatTime(date, value.morningShiftEndHours, value.morningShiftEndMinutes),
+            afternoonShiftStart: this.formatTime(date, value.afternoonShiftStartHours, value.afternoonShiftStartMinutes),
+            afternoonShiftEnd: this.formatTime(date, value.afternoonShiftEndHours, value.afternoonShiftEndMinutes),
+            shifts: this.addTee ? 2 : 1
         }
         console.log(event);
         this.eventService.createEvent(event).then((response: any) => {
             this.showSpinner = false;
+            console.log(response);
             alert("Torneo creado!");
         }).catch((error: any) => {
             this.showSpinner = false;
--- src/app/components/single-event.component.ts
+++ src/app/components/single-event.component.ts
@@ -17,6 +17,8 @@ export class SingleEventComponent {
     user: User;
     showSpinner: boolean = false;
     event: any = new Object();
+    heading: any = [];
+    rows: any = [];
 
     constructor(private authService: AuthService,
                 private eventService: EventsService,
@@ -51,10 +53,41 @@ export class SingleEventComponent {
         this.showSpinner = true;
         this.eventService.getSingleEvent(id).then((snapshot: any) => {
             this.event = snapshot.val();
+            console.log(this.event);
+            this.drawTable(this.event);
             this.showSpinner = false;
         }).catch((error: any) => {
             console.log(error);
             this.showSpinner = false;
         })
+    }
+
+    drawTable(event: any) {
+        for (var i = 1; i < parseInt(event.numberOfPlayers) + 1; i++) {
+            this.heading.push(i);
+        }
+        this.drawShift(event, event.morningShiftStart, event.morningShiftEnd);
+        if (event.afternoonShiftStart > 0) {
+            this.drawShift(event, event.afternoonShiftStart, event.afternoonShiftEnd);
+        }
+
+        console.log(this.heading)
+    }
+
+    drawShift(event: any, startTime: any, endTime: any) {
+        while (startTime < endTime) {
+            var array: any = [];
+            var date = new Date(startTime);
+            array[0] = date.getHours() + ":" + this.formatMinutes(date.getMinutes().toString());
+            for (var i = 1; i < parseInt(event.numberOfPlayers) + 1; i++) {
+                array[i] = "";
+            }
+            this.rows.push(array);
+            startTime += (event.teeTimesInterval * 60000);
+        }
+    }
+
+    formatMinutes(minutes: string) {
+        return minutes.length == 1 ? '0' + minutes : minutes;
     }
 }
